8490d8509d98b38a2fc0ff6cfeea8e9f53c5cdb9
Suan Tonsing <suanasdf@gmail.com>
2020-10-12 19:26:44 +0530

    Loads/Saves data to asyncStorage

--- App.js
+++ App.js
@@ -9,7 +9,13 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 
-import {SafeAreaView, ScrollView, Text, StatusBar} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
 
 export default function App() {
   const [data, setData] = useState([]);
@@ -27,42 +33,97 @@ export default function App() {
       });
   }, []);
 
-  const {first_name} = data;
-  const fn = JSON.stringify(first_name);
+  const apiData = () => {
+    console.log('API Data:');
+    console.log(data);
+  };
 
-  //save data to async storage
-  const storeData = async (value) => {
+  const saveData = async (value) => {
     try {
       const jsonValue = JSON.stringify(value);
-      await AsyncStorage.setItem('firNm', jsonValue);
+      await AsyncStorage.setItem('keyOne', jsonValue);
     } catch (e) {
       // saving error
+    }
+  };
+
+  const loadData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('keyOne');
+      if (value !== null) {
+        // value previously stored
+        console.log(value);
+      } else {
+        console.log('No Key Exists');
+      }
+    } catch (e) {
       console.log(e);
     }
   };
 
-  const getFirstname = async () => {
-    try {
-      const value = await AsyncStorage.getItem('firNm');
-      if (value !== null) {
-        return <Text>{value}</Text>;
-      }
-      return <Text>Name not Available</Text>;
-    } catch (e) {
-      // error reading value
-    }
+  const showKeys = async () => {
+    console.log('Showing Keys');
+    var keys = await AsyncStorage.getAllKeys();
+    console.log(keys);
   };
 
-  //{storeData(first_name)}
-  //{getFirstname()}
+  const clearKeys = () => {
+    console.log('Clearing Data');
+    AsyncStorage.clear();
+    console.log('Cleared Data');
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         <ScrollView>
           <Text>Helloo App</Text>
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={() => apiData()}>
+            <Text>Show API Loaded Data</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={() => saveData(data.first_name)}>
+            <Text>Save Data</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={() => loadData()}>
+            <Text>Load Data</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={() => showKeys()}>
+            <Text>Show Keys</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={() => clearKeys()}>
+            <Text>Clear Keys</Text>
+          </TouchableOpacity>
         </ScrollView>
       </SafeAreaView>
     </>
   );
 }
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+  },
+  buttonStyle: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    margin: 10,
+  },
+};
